c6a4eec635844f9325267b50edd7e1a38db74de0
S525096 <S525096@NW31975>
2016-04-18 13:28:03 -0500

    Added ActorControllerTest

---
+++ src/CodeAgentsTeam3/ActressesController.cs
@@ -0,0 +1,14 @@
+﻿using CodeAgentsTeam3.Models;
+
+namespace CodeAgentsTeam3.Controllers
+{
+    internal class ActressesController
+    {
+        private ApplicationDbContext dbContext;
+
+        public ActressesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
